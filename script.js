f9acb9e604be4f467cf3f690b782a6c44e517fa7
axel-durham <axdurham@gmail.com>
2020-06-20 14:13:17 -0600

    Sort table by selling price

--- script.js
+++ script.js
@@ -48,7 +48,41 @@ var tabulate = function(data, columns) {  // function with 'data' and 'columns' 
 d3.csv('fish.csv', function (data) {
     var columns = ['Name', 'Month_list', 'Month', 'Location', 'Shadow', 'Selling Price', 'Time_list', 'Time']
     tabulate(data, columns)
+    sortTable()
 })
+
+// Adapted from https://www.w3schools.com/howto/howto_js_sort_table.asp
+function sortTable() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementsByClassName("table");
+    switching = true;
+    // Make a loop that will continue until no switching has been done:
+    while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        // Loop through all table rows (except the first, which contains table headers):
+        for (i = 1; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            // Get the two elements you want to compare, one from current row and one from the next:
+            x = rows[i].getElementsByTagName("TD")[5];
+            y = rows[i + 1].getElementsByTagName("TD")[5];
+            // Check if the two rows should switch place:
+            if (parseInt(x.innerHTML) < parseInt(y.innerHTML)) {
+                // If so, mark as a switch and break the loop:
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            // If a switch has been marked, make the switch and mark that a switch has been done:
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
+
 
 // Adapted from https://stackoverflow.com/questions/51515778/how-to-filter-an-html-table-based-on-drop-down-selected-value/51517077 
 // Comments are my own
